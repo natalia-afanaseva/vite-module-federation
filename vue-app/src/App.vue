91a21b5bf63ae386a7d3a79a2769ca73b1c90a37
Natalia A <justnatasha1@gmail.com>
2025-04-05 14:29:48 -0700

    vite vue nuxt

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import img from './assets/vue.svg'
+import { ref, defineAsyncComponent } from 'vue';
+// import img from './assets/vue.svg'
 
-onMounted(async () => {
-  const remoteApp = await import('remote/Navbar');
-  remoteApp.mount(document.getElementById('nav'))
-  const remoteCard = await import('remote/Card');
-  remoteCard.mount(document.getElementById('card'), {
-     img,
-      title: "A title from Vue host",
-      description: "Description from Vue host"
-  })
-})
+// onMounted(async () => {
+//   const remoteApp = await import('remote/Navbar');
+//   remoteApp.mount(document.getElementById('nav'))
+//   const remoteCard = await import('remote/Card');
+//   remoteCard.mount(document.getElementById('card'), {
+//      img,
+//       title: "A title from Vue host",
+//       description: "Description from Vue host"
+//   })
+// })
+
+const RemoteComponent = defineAsyncComponent(() =>
+  import('remoteNuxt/app')
+);
+
+const counter = ref(0);
 </script>
 
 <template>
   <div>
     <div id="nav"></div>
+    <RemoteComponent :counter="counter" />
+    <button @click="counter++">Click</button>
     <main class="container">
       <h1>Vue + Vite</h1>
       <div id="card"></div>
@@ -25,5 +33,5 @@ onMounted(async () => {
 </template>
 
 <style>
-@import url('http://localhost:5172/index.css');
+/* @import url('http://localhost:5172/index.css'); */
 </style>
--- vue-app/vite.config.ts
+++ vue-app/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { federation } from "@module-federation/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,15 +13,39 @@ export default defineConfig({
     federation({
       name: "host-app",
       remotes: {
-        remote: {
-          entry: "http://localhost:5172/remoteEntry.js",
+        // remote: {
+        //   entry: "http://localhost:5172/remoteEntry.js",
+        //   type: "module",
+        //   name: "remote",
+        // },
+        remoteNuxt: {
+          entry: "http://127.0.0.1:3000/remoteEntry.js",
           type: "module",
-          name: "remote",
+          name: "remoteNuxt",
+        },
+      },
+      shared: {
+        vue: {
+          singleton: true,
+          requiredVersion: "^3.5.13",
         },
       },
     }),
   ],
   build: {
     target: "esnext",
+  },
+  server: {
+    cors: {
+      allowedHeaders: "*",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  resolve: {
+    alias: {
+      vue: path.resolve(__dirname, "node_modules/vue"),
+    },
   },
 });
